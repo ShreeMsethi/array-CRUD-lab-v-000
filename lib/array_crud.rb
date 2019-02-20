01e76f048c47c5dda4b60d58fa2bd112a68a7918
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-02-20 00:22:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(best_muscle_cars,corvette)
-  best_muscle_cars.push("corvette")
+  best_muscle_cars.push
 end
 
 def add_element_to_start_of_array(best_muscle_cars,ferrari)
-  best_muscle_cars.unshift("ferrari")
+  best_muscle_cars.unshift
 end
 
 def remove_element_from_end_of_array(best_muscle_cars)
