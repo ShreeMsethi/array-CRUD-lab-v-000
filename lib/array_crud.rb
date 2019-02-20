9a759c7449519149e04fdabd9572b6b1282d90ce
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-02-20 00:42:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   best_muscle_cars = ["mustang", "camero", "challenger", "charger"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(best_muscle_cars, corvette)
+  best_muscle_cars.push("corvette")
 end
 
 def add_element_to_start_of_array(array, element)
