912ea9d890a1a14249e1c271b271d72538a428fb
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-02-20 00:48:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(best_muscle_cars, car_model)
-  best_muscle_cars.push(corvette)
+  best_muscle_cars.push('corvette')
 end
 
 def add_element_to_start_of_array(array, element)
